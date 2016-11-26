dda9077e109001340f9e07f4693aaa2ce1d390f2
Bartosz Marcinkowski <bm371613@gmail.com>
2016-11-26 17:37:04 +0100

    mcts child selector

--- quoridor-ai/src/main/java/quoridor/ai/bot/mcts/ChildSelector.java
+++ quoridor-ai/src/main/java/quoridor/ai/bot/mcts/ChildSelector.java
@@ -1,16 +1,25 @@
 package quoridor.ai.bot.mcts;
 
 public interface ChildSelector {
-    float evaluateChild(Node child, int playerIx);
 
-    default Node selectChild(Node node) {
-        int playerIx = node.getGameState().currentPlayerIx();
-        float bestValue = Float.MIN_VALUE;
-        float currentValue;
+    double hopeFactor(double childrenSimulationCount);
+
+    default Node selectChild(Node parent) {
+        int playerIx = parent.getGameState().currentPlayerIx();
+        double bestValue = Float.MIN_VALUE;
+        double currentValue;
         Node bestChild = null;
+        double hopeFactor = hopeFactor(parent.getChildrenSimulationCount());
+        int childSimulationCount;
 
-        for (Node child : node.getChildren()) {
-            currentValue = evaluateChild(child, playerIx);
+        for (Node child : parent.getChildren()) {
+            childSimulationCount = child.getSimulationCount();
+            if (childSimulationCount == 0) {
+                return child;
+            }
+            currentValue = ((double) child.getWinCount()[playerIx])
+                    / childSimulationCount
+                    + Math.sqrt(hopeFactor / child.getSimulationCount());
             if (currentValue > bestValue) {
                 bestChild = child;
                 bestValue = currentValue;
@@ -19,4 +28,7 @@ public interface ChildSelector {
 
         return bestChild;
     }
+
+    ChildSelector WITH_LOG_HOPE = Math::log;
+    ChildSelector WITH_SQRT_HOPE = Math::sqrt;
 }
